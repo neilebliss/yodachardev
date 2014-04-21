00ae9c7ca0109b9b8a7ac5d49a325850c97148c4
Neil Bliss <bliss@lab126.com>
2014-04-21 13:51:26 -0700

    initial commit

---
+++ yoda_char_dev.c
@@ -0,0 +1,54 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/fs.h>
+#include "yoda_char_dev.h"
+
+MODULE_LICENSE("Dual MIT/GPL");
+
+#define DEVICE_NAME "yodachardev"
+
+static int Major;
+static struct file_operations fops = {
+	.open = yoda_char_dev_open,
+	.release = yoda_char_dev_release
+};
+
+int __init yoda_char_dev_entry(void)
+{
+	Major = register_chrdev(0, "yodachardev", &fops);
+	if (Major < 0)
+	{
+		printk(KERN_ALERT "Error registering yoda_char_dev\n");
+		return Major;
+	}
+	else
+	{
+		printk(KERN_INFO "Registered yodachardev at major number %d.\n", Major);
+		printk(KERN_INFO "Be sure to set up a device node via something like \"mknod /dev/yodachardev c %d 0\"\n", Major);
+		return 0;
+	}
+}
+
+module_init(yoda_char_dev_entry);
+
+void __exit yoda_char_dev_exit(void)
+{
+	unregister_chrdev(Major, DEVICE_NAME);
+	printk(KERN_INFO "yoda_char_dev shutting down.\n");
+}
+
+module_exit(yoda_char_dev_exit);
+
+static int yoda_char_dev_open(struct inode *inode, struct file *file)
+{
+	printk(KERN_INFO "yoda_char_dev_open\n");
+	try_module_get(THIS_MODULE);
+	return 0;
+}
+
+static int yoda_char_dev_release(struct inode *inode, struct file *file)
+{
+	module_put(THIS_MODULE);
+	printk(KERN_INFO "yoda_char_dev_release\n");
+	return 0;
+}
---
+++ yoda_char_dev.h
@@ -0,0 +1,7 @@
+#ifndef YODA
+#define YODA
+int yoda_char_dev_entry(void);
+void yoda_char_dev_exit(void);
+static int yoda_char_dev_open(struct inode *, struct file *);
+static int yoda_char_dev_release(struct inode *, struct file *);
+#endif
