5cebccc0f5b9f42752bd645c9d2c2e0e4d969f73
Neil Bliss <bliss@lab126.com>
2014-04-22 11:15:06 -0700

    ghost removal

--- yoda_char_dev.h
+++
@@ -1,8 +0,0 @@
-#ifndef YODA
-#define YODA
-int yoda_char_dev_entry(void);
-void yoda_char_dev_exit(void);
-static int yoda_char_dev_open(struct inode *, struct file *);
-static int yoda_char_dev_release(struct inode *, struct file *);
-#define DEVICE_NAME "yodachardev"
-#endif
