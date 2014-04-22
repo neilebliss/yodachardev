0984c7b4de27d0ca6dbdb9a0076e5aa8c460fef0
Neil Bliss <bliss@lab126.com>
2014-04-22 12:15:58 -0700

    fix up procfile read.

--- src/yoda_char_dev_procfile_read.c
+++ src/yoda_char_dev_procfile_read.c
@@ -2,6 +2,13 @@
 
 int yoda_char_dev_procfile_read(char *buffer, char **buffer_location, off_t offset, int buffer_length, int *eof, void *data)
 {
-	sprintf(buffer, "Do, or do not.  There is no try!");
-	return 0;
+	int ret = 0;
+	char *msg = "Do, or do not.  There is no try!";
+	printk(KERN_INFO "%s trying to print via procfile...\n", DEVICE_NAME);
+	if (offset > 0)
+	{
+		return 0;
+	}
+	ret = sprintf(buffer, msg);
+	return ret;
 }
