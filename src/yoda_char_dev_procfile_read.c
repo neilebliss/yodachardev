44c8ed5158428d35f1e618f88696244041377eed
Neil Bliss <bliss@lab126.com>
2014-04-22 14:27:42 -0700

    more procfile cleanup.

--- src/yoda_char_dev_procfile_read.c
+++ src/yoda_char_dev_procfile_read.c
@@ -4,11 +4,13 @@ int yoda_char_dev_procfile_read(char *buffer, char **buffer_location, off_t offs
 {
 	int bytes_sent = 0;
 	static int message_sent_index = 0;
-	const char *msg = "Do, or do not.  There is no try!";
+	const char *msg = "Do, or do not.  There is no try!\n";
 	int msg_length = strlen(msg);
 	printk(KERN_INFO "%s trying to print via procfile...\n", DEVICE_NAME);
 	if (offset > 0 && offset <= (msg_length - 1))
 	{
+		// reset the message index counter to resend the message fully next time.
+		message_sent_index = 0;
 		return 0;
 	}
 	bytes_sent = snprintf(buffer, (msg_length - offset), msg + offset);
